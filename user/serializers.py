c9cc3cb119f1d1d3fedfb3633d7f92aec4f36f7b
Maksym Dazidov <maksymdazidov@gmail.com>
2026-04-09 21:09:32 +0200

    feat(user): implement serializer for user model

---
+++ user/serializers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'first_name', 'last_name', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'min_length': 8,
+                'style': {'input_type': 'password'},
+                'trim_whitespace': False,
+            }
+        }
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
