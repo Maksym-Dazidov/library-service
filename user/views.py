d998b080856738169e3e2aa97a564e8ab4f386b3
Maksym Dazidov <maksymdazidov@gmail.com>
2026-04-09 21:10:39 +0200

    feat(user): implement views for user model

--- user/views.py
+++ user/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+User = get_user_model()
+
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+
+
+class MeView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
